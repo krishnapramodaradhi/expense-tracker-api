2f0cc3f8b8ce464c35239d8042ea5bdf4f33d94a
Krishna Pramod <krishna.pramod2@gmail.com>
2024-03-23 01:30:50 +0530

    Added github workflow

--- cmd/expense-tracker/main.go
+++ cmd/expense-tracker/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/krishnapramodaradhi/expense-tracker-api/internal/config"
 )
 
@@ -12,12 +10,6 @@ func init() {
 }
 
 func main() {
-	log.Println(os.Getenv("PORT"), os.Getenv("APP_ENV"))
-	if os.Getenv("APP_ENV") != "prod" {
-		if err := godotenv.Load(); err != nil {
-			log.Fatal("There was an error while loading .env variables", err)
-		}
-	}
 	server := config.NewServer(os.Getenv("PORT"), nil)
 	server.Run()
 }
--- go.mod
+++ go.mod
@@ -2,8 +2,4 @@ module github.com/krishnapramodaradhi/expense-tracker-api
 
 go 1.22.1
 
-require (
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-)
+require github.com/gorilla/mux v1.8.1
